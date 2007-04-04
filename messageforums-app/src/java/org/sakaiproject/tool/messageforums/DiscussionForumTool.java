05ab8aceccd4f69c6766da78ebebc92578adb8bc
ajpoland@iupui.edu <ajpoland@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-04-04 18:36:06 +0000

    svn merge -c 28269 https://source.sakaiproject.org/svn/msgcntr/trunk U    messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java U    messageforums-app/src/webapp/jsp/discussionForum/message/dfViewThreadBodyInclude.jsp svn log -r 28269 https://source.sakaiproject.org/svn/msgcntr/trunk ------------------------------------------------------------------------ r28269 | rjlowe@iupui.edu | 2007-04-03 10:40:20 -0400 (Tue, 03 Apr 2007) | 3 lines
    SAK-9195
    http://bugs.sakaiproject.org/jira/browse/SAK-9195
    Attachments are not displaying in Entire Message View
    ------------------------------------------------------------------------
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/sakai_2-4-x@28321 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -2146,7 +2146,7 @@ public class DiscussionForumTool
        
     }
 
-    List temp_messages = forumManager.getTopicByIdWithMessages(topic.getId())
+    List temp_messages = forumManager.getTopicByIdWithMessagesAndAttachments(topic.getId())
         .getMessages();
     if (temp_messages == null || temp_messages.size() < 1)
     {
