540c18f81f4a2d02388ccd78b04fac19c393db47
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-08-23 12:29:29 +0000

    SAK-11228 UCT inmprovements it forums SAK-5274 user getDisplayName for name lookups SAK-11229 Highlight threads with unread messages SAK-11177 correct message counts SAK-7074 Optionaly supress forum level links if mc.showForumLinksInNav is set in sakai.properties SAK-11173 add icons for topics distinguishes topics with read and unread messages
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/SAK-11228@34280 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -218,6 +218,8 @@ public class DiscussionForumTool
   private List pendingMsgs = new ArrayList();
   
   private String userId;
+  
+  private boolean showForumLinksInNav = true;
 
   // compose
   private MessageForumsMessageManager messageManager;
@@ -296,6 +298,8 @@ public class DiscussionForumTool
     {
     	disableLongDesc = true;
     }
+    
+    showForumLinksInNav = ServerConfigurationService.getBoolean("mc.showForumLinksInNav", true);
   }
 
   /**
@@ -5504,41 +5508,21 @@ public class DiscussionForumTool
 	  try
 	  {
 		String currentUserId = getUserId();
-  	    if(!"false".equalsIgnoreCase(ServerConfigurationService.getString
-		  ("separateIdEid@org.sakaiproject.user.api.UserDirectoryService")))
-  	    {
+  	    
+  	    
 		  String userString = "";
-		  String userLastName = UserDirectoryService.getUser(currentUserId).getLastName();
-		  String userFirstName = UserDirectoryService.getUser(currentUserId).getFirstName();
-		  if((userLastName != null && userLastName.length() > 0) ||
-		    (userFirstName != null && userFirstName.length() > 0))
+		  userString = UserDirectoryService.getUser(currentUserId).getDisplayName();
+		  
+		  if((userString != null && userString.length() > 0))
 		  {
-			userString += userLastName + ", ";
-			userString += userFirstName;
+
 			return userString;
 		  }
 		  else
 		  {
-			return UserDirectoryService.getUserEid(currentUserId);
+			return UserDirectoryService.getUser(currentUserId).getDisplayId();
 		  }
-		}
-  	    else
-  	    {
-  	      String userString = "";
-  	      String userLastName = UserDirectoryService.getUser(currentUserId).getLastName();
-		  String userFirstName = UserDirectoryService.getUser(currentUserId).getFirstName();
-		  if((userLastName != null && userLastName.length() > 0) ||
-		    (userFirstName != null && userFirstName.length() > 0))
-		  {
-			userString += userLastName + ", ";
-			userString += userFirstName;
-			return userString;
-		  }
-		  else
-		  {
-			return UserDirectoryService.getUserEid(currentUserId);
-		  }
-  	    }
+		
 	  }
   	  catch(Exception e)
   	  {
@@ -5792,5 +5776,9 @@ public class DiscussionForumTool
 	 }
 	 public boolean isNoItemSelected() {
 		 return selectedAssign == null || selectedAssign.equalsIgnoreCase(DEFAULT_GB_ITEM);
+	 }
+	 
+	 public boolean getShowForumLinksInNav() {
+		 return showForumLinksInNav;
 	 }
 }
