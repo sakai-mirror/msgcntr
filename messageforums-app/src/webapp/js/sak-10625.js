3b950ba35e2a11fdca34ab3d051e379bbb806f32
josrodri@iupui.edu <josrodri@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-09-21 19:15:34 +0000

    tweaking Javascript to remove an IE specific error with code for SAK-10625, possibly fixing IE locking up in Messages (IU local issue)
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/oncourse_2-4-x@35655 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/webapp/js/sak-10625.js
+++ messageforums-app/src/webapp/js/sak-10625.js
@@ -3,6 +3,8 @@ sak10625_disabler = function(){
    $("a, input[@type=button], input[@type=submit]").attr("onclick", "");
 }
 $(document).ready(function(){
-	$("a").filter(function(){regexp=/submit\(\)/; return regexp.test($(this).attr("onclick"));}).bind("click", sak10625_disabler);
+	$("a").filter(function(){regexp=/submit\(\)/; 
+							 var obj = regexp.test($(this).attr("onclick"));
+							 if (obj) obj.bind("click", sak10625_disabler);})
    $("input[@type=button], input[@type=submit]").bind("click", sak10625_disabler);
 });
