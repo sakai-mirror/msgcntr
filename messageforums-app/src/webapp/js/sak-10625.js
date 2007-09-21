7b082ccea06fb33dd540aeccbc7b74dcfe5142d1
josrodri@iupui.edu <josrodri@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-09-21 20:28:16 +0000

    ONC-178: actual fix for javascript error at point users have locked up
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/oncourse_2-4-x@35664 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/webapp/js/sak-10625.js
+++ messageforums-app/src/webapp/js/sak-10625.js
@@ -3,8 +3,7 @@ sak10625_disabler = function(){
    $("a, input[@type=button], input[@type=submit]").attr("onclick", "");
 }
 $(document).ready(function(){
-	$("a").filter(function(){regexp=/submit\(\)/; return regexp.test($(this).attr("onclick"));}).bind("click", sak10625_disabler);
+	var obj = $("a").filter(function(){regexp=/submit\(\)/; return regexp.test($(this).attr("onclick"));})
+	if (obj) obj.bind("click", sak10625_disabler);
    $("input[@type=button], input[@type=submit]").bind("click", sak10625_disabler);
 });
-
-}
