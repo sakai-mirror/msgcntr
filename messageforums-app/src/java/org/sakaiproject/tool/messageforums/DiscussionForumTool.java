12912261bdd5721db72dc32128be561a67c89c4a
jean-francois.leveque@upmc.fr <jean-francois.leveque@upmc.fr@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-03-24 14:02:16 +0000

    svn log -r42565:42566 https://source.sakaiproject.org/svn/msgcntr/trunk ------------------------------------------------------------------------ r42566 | wang58@iupui.edu | 2008-03-20 19:16:03 +0100 (jeu, 20 mar 2008) | 1 line
    SAK-13242 Forums/topic settings-attachment
    ------------------------------------------------------------------------
    leveque@jfl149:~/sakaiBranchManagement/2.5.x$ svn merge -r42565:42566 https://source.sakaiproject.org/svn/msgcntr/trunk msgcntr/
    U    msgcntr/messageforums-app/src/webapp/jsp/discussionForum/message/dfAllMessages.jsp
    U    msgcntr/messageforums-app/src/webapp/jsp/discussionForum/forum/dfForumDetail.jsp
    leveque@jfl149:~/sakaiBranchManagement/2.5.x$ svn log -r45223:45224 https://source.sakaiproject.org/svn/msgcntr/trunk
    ------------------------------------------------------------------------
    r45224 | wang58@iupui.edu | 2008-04-07 15:42:40 +0200 (lun, 07 avr 2008) | 2 lines
    
    SAK-13242 Forums/topic settings-attachment.
    fix LazyInitializationException when create new thread in Forums.
    ------------------------------------------------------------------------
    leveque@jfl149:~/sakaiBranchManagement/2.5.x$ svn merge -r45223:45224 https://source.sakaiproject.org/svn/msgcntr/trunk msgcntr/
    U    msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/MessageForumsForumManagerImpl.java
    U    msgcntr/messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/ui/DiscussionForumManagerImpl.java
    U    msgcntr/messageforums-hbm/src/java/org/sakaiproject/component/app/messageforums/dao/hibernate/Topic.hbm.xml
    U    msgcntr/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/MessageForumsForumManager.java
    U    msgcntr/messageforums-api/src/java/org/sakaiproject/api/app/messageforums/ui/DiscussionForumManager.java
    U    msgcntr/messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
    
    
    
    
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/sakai_2-5-x@59055 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -2742,7 +2742,7 @@ public class DiscussionForumTool
     	return gotoMain();
     }
     forumManager.saveMessage(dMsg);
-    DiscussionTopic dSelectedTopic = (DiscussionTopic) forumManager.getTopicByIdWithMessages(selectedTopic.getTopic().getId());
+    DiscussionTopic dSelectedTopic = (DiscussionTopic) forumManager.getTopicWithAttachmentsById(selectedTopic.getTopic().getId());
     setSelectedForumForCurrentTopic(dSelectedTopic);
     selectedTopic.setTopic(dSelectedTopic);
     selectedTopic.getTopic().setBaseForum(selectedForum.getForum());
