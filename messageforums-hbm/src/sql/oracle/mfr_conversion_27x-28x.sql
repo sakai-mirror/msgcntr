84961d1a2ee4050d489b5810022846b05d8325bf
wagnermr@iupui.edu <wagnermr@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-11-20 02:00:45 +0000

    svn merge -c85047 https://source.sakaiproject.org/svn/msgcntr/trunk --- Merging r85047 into '.': U    messageforums-hbm/src/sql/oracle/mfr_conversion_27x-28x.sql ------------------------------------------------------------------------ r85047 | chmaurer@iupui.edu | 2010-11-17 16:36:34 -0500 (Wed, 17 Nov 2010) | 3 lines
    MSGCNTR-407
    Fix some of the conversion
    
    ------------------------------------------------------------------------
    svn merge -c85083 https://source.sakaiproject.org/svn/msgcntr/trunk
    --- Merging r85083 into '.':
    G    messageforums-hbm/src/sql/oracle/mfr_conversion_27x-28x.sql
    ------------------------------------------------------------------------
    r85083 | chmaurer@iupui.edu | 2010-11-18 10:38:36 -0500 (Thu, 18 Nov 2010) | 3 lines
    
    MSGCNTR-407
    Fix up the conversion for oracle
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/msgcntr-2.8.x@85243 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-hbm/src/sql/oracle/mfr_conversion_27x-28x.sql
+++ messageforums-hbm/src/sql/oracle/mfr_conversion_27x-28x.sql
@@ -2,11 +2,11 @@
 --Start and End dates on Forums and Topics
 alter table MFR_AREA_T add (AVAILABILITY_RESTRICTED NUMBER(1,0));
 update MFR_AREA_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_AREA_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) not null default false);
+alter table MFR_AREA_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) default 0 not null );
 
 alter table MFR_AREA_T add (AVAILABILITY NUMBER(1,0));
 update MFR_AREA_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_AREA_T modify (AVAILABILITY NUMBER(1,0) not null default false);
+alter table MFR_AREA_T modify (AVAILABILITY NUMBER(1,0) default 0 not null);
 
 alter table MFR_AREA_T add (OPEN_DATE timestamp);
 
@@ -15,11 +15,11 @@ alter table MFR_AREA_T add (CLOSE_DATE timestamp);
 
 alter table MFR_OPEN_FORUM_T add (AVAILABILITY_RESTRICTED NUMBER(1,0));
 update MFR_OPEN_FORUM_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_OPEN_FORUM_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) not null default false);
+alter table MFR_OPEN_FORUM_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) default 0 not null );
 
 alter table MFR_OPEN_FORUM_T add (AVAILABILITY NUMBER(1,0));
 update MFR_OPEN_FORUM_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_OPEN_FORUM_T modify (AVAILABILITY NUMBER(1,0) not null default false);
+alter table MFR_OPEN_FORUM_T modify (AVAILABILITY NUMBER(1,0) default 0 not null );
 
 alter table MFR_OPEN_FORUM_T add (OPEN_DATE timestamp);
 
@@ -27,11 +27,11 @@ alter table MFR_OPEN_FORUM_T add (CLOSE_DATE timestamp);
 
 alter table MFR_TOPIC_T add (AVAILABILITY_RESTRICTED NUMBER(1,0));
 update MFR_TOPIC_T set AVAILABILITY_RESTRICTED=0 where AVAILABILITY_RESTRICTED is NULL;
-alter table MFR_TOPIC_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) not null default false);
+alter table MFR_TOPIC_T modify (AVAILABILITY_RESTRICTED NUMBER(1,0) default 0 not null );
 
 alter table MFR_TOPIC_T add (AVAILABILITY NUMBER(1,0));
 update MFR_TOPIC_T set AVAILABILITY=0 where AVAILABILITY is NULL;
-alter table MFR_TOPIC_T modify (AVAILABILITY NUMBER(1,0) not null default false);
+alter table MFR_TOPIC_T modify (AVAILABILITY NUMBER(1,0) default 0 not null );
 
 alter table MFR_TOPIC_T add (OPEN_DATE timestamp);
 
@@ -39,15 +39,14 @@ alter table MFR_TOPIC_T add (CLOSE_DATE timestamp);
 
 
 --MSGCNTR-355
-insert into MFR_TOPIC_T (UUID, MODERATED, AUTO_MARK_THREADS_READ, SORT_INDEX, MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID)
+insert into MFR_TOPIC_T (ID, UUID, MODERATED, AUTO_MARK_THREADS_READ, SORT_INDEX, MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID)
 
-    select UUID, MODERATED, AUTO_MARK_THREADS_READ, 3 as SORT_INDEX, 0 as MUTABLE, TOPIC_DTYPE, VERSION, CREATED, CREATED_BY, MODIFIED, MODIFIED_BY, TITLE, SHORT_DESCRIPTION, EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID from (
-                    select count(*) as c1, SYS_GUID() as UUID, mtt.MODERATED, mtt.AUTO_MARK_THREADS_READ, mtt.TOPIC_DTYPE, 0 as VERSION, mtt.CREATED, mtt.CREATED_BY, mtt.MODIFIED, mtt.MODIFIED_BY, 'pvt_drafts' as TITLE, 'short-desc' as SHORT_DESCRIPTION, 'ext-desc' as EXTENDED_DESCRIPTION, mtt.TYPE_UUID, mtt.pf_surrogateKey, mtt.USER_ID
-                    from MFR_PRIVATE_FORUM_T mpft, MFR_TOPIC_T mtt
-                    where mpft.ID = mtt.pf_surrogateKey and mpft.TYPE_UUID = mtt.TYPE_UUID
-                    Group By mtt.USER_ID, mtt.pf_surrogateKey) s1
-    where s1.c1 = 3;
-    
+	(select MFR_TOPIC_S.nextval as ID, sys_guid() as UUID, MODERATED, 0 as AUTO_MARK_THREADS_READ, 3 as SORT_INDEX, 0 as MUTABLE, TOPIC_DTYPE, 0 as VERSION, sysdate as CREATED, CREATED_BY, sysdate as MODIFIED, MODIFIED_BY, 'pvt_drafts' as TITLE, 'short-desc' as SHORT_DESCRIPTION, 'ext-desc' as EXTENDED_DESCRIPTION, TYPE_UUID, pf_surrogateKey, USER_ID from (
+		select count(*) as c1, mtt.MODERATED, mtt.TOPIC_DTYPE, mtt.CREATED_BY, mtt.MODIFIED_BY, mtt.TYPE_UUID, mtt.pf_surrogateKey, mtt.USER_ID
+		from MFR_PRIVATE_FORUM_T mpft, MFR_TOPIC_T mtt
+		where mpft.ID = mtt.pf_surrogateKey and mpft.TYPE_UUID = mtt.TYPE_UUID
+		Group By mtt.USER_ID, mtt.pf_surrogateKey, mtt.MODERATED, mtt.TOPIC_DTYPE, mtt.CREATED_BY, mtt.MODIFIED_BY, mtt.TYPE_UUID) s1
+	where s1.c1 = 3);
     
 
 --MSGCNTR-360
