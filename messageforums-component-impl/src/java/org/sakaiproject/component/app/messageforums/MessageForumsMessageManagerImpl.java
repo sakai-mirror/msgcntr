cccfe5fbe3d7034b9b6fe92915c9dd8421766267
holladay@longsight.com <holladay@longsight.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2011-01-13 14:05:27 +0000

    MSGCNTR-359 StaleObjectStateException: High concurrency updates leading to stale Hibernate objects
    merging into 2.7.x
    
    
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/msgcntr-2.7.x@87172 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/MessageForumsMessageManagerImpl.java
+++ messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/MessageForumsMessageManagerImpl.java
@@ -1094,12 +1094,6 @@ public class MessageForumsMessageManagerImpl extends HibernateDaoSupport impleme
 
         getHibernateTemplate().saveOrUpdate(message);
 
-				try {
-            getSession().flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    
         if (logEvent) {
         	if (isNew) {
         		if (isMessageFromForums(message))
@@ -1111,29 +1105,7 @@ public class MessageForumsMessageManagerImpl extends HibernateDaoSupport impleme
         			eventTrackingService.post(eventTrackingService.newEvent(DiscussionForumService.EVENT_FORUMS_RESPONSE, getEventMessage(message), false));
         		else
         			eventTrackingService.post(eventTrackingService.newEvent(DiscussionForumService.EVENT_MESSAGES_RESPONSE, getEventMessage(message), false));
-        	}
-            
-        	//we don't need to do this on non log events
-        	message.setDateThreadlastUpdated(new Date());
-            if (message.getInReplyTo() != null) {
-            	message.setThreadId(message.getInReplyTo().getThreadId());
-            }
-            if (message.getInReplyTo() != null) {
-            	Message m = null;
-            	if (message.getThreadId() != null)
-            		m = this.getMessageById(message.getThreadId());
-            	else 
-            		m = message.getInReplyTo();
-            	//otherwise we get an NPE when we try to save this message
-            	Topic topic = message.getTopic();
-            	BaseForum bf  = topic.getBaseForum();
-            	m.setTopic(topic);
-            	m.getTopic().setBaseForum(bf);
-            	m.setThreadLastPost(message.getId());
-            	m.setDateThreadlastUpdated(new Date());
-            	this.saveMessage(m, false, ignoreLockedTopicForum);
-            }
-            
+	        }
         }
         
         LOG.info("message " + message.getId() + " saved successfully");
