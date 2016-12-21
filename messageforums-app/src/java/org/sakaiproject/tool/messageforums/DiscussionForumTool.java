992352108d30f43650268b4ecc9e81b2bd04bf3d
matthew@longsight.com <matthew@longsight.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2016-12-21 23:52:10 +0000

    Revert - SAK-25289 - Merge PR 3594 from master - Breadcrumbs trail incorrect when accessing forum topic
    git-svn-id: https://source.sakaiproject.org/svn/msgcntr/branches/sakai-10.x@325633 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
+++ messageforums-app/src/java/org/sakaiproject/tool/messageforums/DiscussionForumTool.java
@@ -7045,6 +7045,10 @@ public class DiscussionForumTool
    */
   private void setSelectedForumForCurrentTopic(DiscussionTopic topic)
   {
+    if (selectedForum != null)
+    {
+      return;
+    }
     DiscussionForum forum = (DiscussionForum) topic.getBaseForum();
     if (forum == null)
     {
